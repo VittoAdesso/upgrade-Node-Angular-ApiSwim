af6a08caa2cc68f88122d8206af7a04e89c0451c
Vittorio Adesso <adessovittorio8@gmail.com>
2022-01-05 22:31:55 +0100

    Estilos espalda

--- server/api/seeds/espalda.seed.js
+++ server/api/seeds/espalda.seed.js
@@ -9,10 +9,39 @@ const Espalda = require("../models/Espalda");
 // es mi array que debo ir construyendo
 const espalda = [{
 
-    "title": "Espalda",
-    "img": "https://rdv-files.nyc3.cdn.digitaloceanspaces.com/pub/html/files_html/3/4/6/000803461.png",
-    "description": "Nado de espalda principal"
+    "title": "Espalda - crol de espalda",
+    "img": "https://www.liveabout.com/thmb/xjBv_lCrXS9D4Sih0RDeAOYxYOw=/2119x1192/smart/filters:no_upscale()/GettyImages-519665498-5c6f88ff46e0fb0001076287.jpg",
+    "description": "El estilo de natación de espalda es llamado también 'crol de espalda' debido a que en algún tiempo se le consideró una versión invertida del crol de frente. Efectivamente, las semejanzas entre ambos estilos son notables cuando se estudia detenidamente sus principios mecánicos. El crol de espalda es el tercer estilo más rápido, después de el crol y la mariposa. Aunque las tablas de récords señalan unas marcas, aparentemente, muy similares con respecto al estilo mariposa (50m. mariposa = 22.96 vs. 50m. espalda = 24.47), existe una diferencia de casi un segundo y medio a favor de la mariposa, lo cual representa demasiado tiempo si hablamos de alta competición, donde cada décima de segundo tiene mucha importancia."
     // "video": ""
+
+}, 
+{
+    "title": "Básicos",
+    "img": "https://www.swim-teach.com/images/backstroke-timing.jpg",
+    "description": "Como en el crol de frente, el estilo espalda consiste en una acción alternativa de los brazos y de las piernas. Sin embargo, a diferencia del crol, los nadadores están sobre sus espaldas, en posición dorsal. Esto les obliga a realizar las brazadas con una trayectoria más lateral que por debajo del agua, como sucede en el crol."
+    // "video": ""
+
+},
+{
+
+    "title": "Movimientos a ejecutar",
+    "img": "https://i.pinimg.com/736x/01/b9/f1/01b9f1d8b19cf5515ae63fa3eee357a1--swimming-rules-swimming-sport.jpg",
+    "description": "Característica que comparte con el estilo crol es que el cuerpo gira a través de su eje longitudinal, excepto la cabeza que permanece fija; es lo que conoce como rolido pero en posición supina. El reglamento actual de natación exige que el nadador de espalda permanezca sobre su espalda mientras nada y no supere este giro o rolido más de 90 grados."
+    // "video": ""
+
+}, 
+{
+    "title": "Tips",
+    "img": "https://content.artofmanliness.com/uploads/2016/06/Front-Crawl-Stroke-3.jpg",
+    "description": "Es el único estilo en el que la cara nunca queda sumergida en el agua, excepto en los virajes y las salidas. Por este motivo, algunas publicaciones y/o autores sobre natación afirman que el ritmo respiratorio no es tan importante como en otros estilos. Sin embargo, el coordinar la respiración con los movimientos de brazos es fundamental, como en otros estilos, para que la fatiga surja lo más tarde posible."
+    // "video": ""
+}, 
+{
+    "title": "Músculos que trabajamos",
+    "img": "https://i.ytimg.com/vi/OTBtx8dkstw/hqdefault.jpg",
+    "description": "Pectoral, Tríceps, hombros, trapecio, , dorsales, abdomen."
+    // "video": ""
+
 
 }]; 
 
